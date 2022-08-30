b1cf9311caaede79d5941f2880e529534bbea3a8
Casper Laursen <casper.aagaard.laursen@lego.com>
2022-08-30 20:48:34 +0200

    implemented sorting and improved table

--- src/frontend/table/lib.tsx
+++ src/frontend/table/lib.tsx
@@ -3,12 +3,11 @@ import { SuperTableColumn } from './supertable/superTable';
 export interface Player {
   _id: string;
   email: string;
-  ratings: {
-    single: number;
-    double: number;
-  };
   name: string;
-  masterRating: number;
+  ranks: {
+    single: string;
+    double: string;
+  };
 }
 
 export interface PlayerProps {
@@ -34,14 +33,43 @@ export const BasicColumns: SuperTableColumn<Player>[] = [
     width: 'auto',
   },
   {
-    title: 'Single Rating',
+    title: 'Single',
     dataIndex: 'ranks.single',
-    width: 'auto',
     sorted: 'descending',
+    sorter: (a, b) => sortRank(a.ranks.single, b.ranks.single),
   },
   {
-    title: 'Double Rating',
+    title: 'Double',
     dataIndex: 'ranks.double',
-    width: 'auto',
+    sorter: (a, b) => sortRank(a.ranks.double, b.ranks.double),
   },
 ];
+
+function sortRank(a: string, b: string) {
+  const [divisonA, tierA, uncertaintyA] = a.split(' ');
+  const [divisonB, tierB, uncertaintyB] = b.split(' ');
+
+  const divisionToInt: { [div in string]: number } = {
+    Bronze: 0,
+    Silver: 1,
+    Gold: 2,
+    Platinum: 3,
+    Diamond: 4,
+  };
+
+  if (uncertaintyA !== uncertaintyB) {
+    if (!uncertaintyA) {
+      return -1;
+    }
+    if (!uncertaintyB) {
+      return 1;
+    }
+    return uncertaintyB.length - uncertaintyA.length;
+  }
+
+  if (divisonA !== divisonB) {
+    return (divisionToInt[divisonA] || 0) - (divisionToInt[divisonB] || 0);
+  }
+
+  return Number(tierA) - Number(tierB);
+}
